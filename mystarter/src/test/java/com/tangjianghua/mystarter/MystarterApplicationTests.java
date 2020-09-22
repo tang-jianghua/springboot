44181a8a2b36597bd07fbed1ca6804baeacb4e43
tangjianghua <379170882@qq.com>
2020-09-22 21:38:51 +0800

    add

---
+++ mystarter/src/test/java/com/tangjianghua/mystarter/MystarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangjianghua.mystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
